6801ed056f755c9eb8903894fdbbcbe7b9136ae2
MateuszStępkowski <stepkowski.mat@gmail.com>
2018-09-11 20:36:59 +0200

    basic FakerService created +  examplary FakerResource

--- src/main/java/pl/coderslab/config/AppConfig.java
+++ src/main/java/pl/coderslab/config/AppConfig.java
@@ -2,12 +2,14 @@ package pl.coderslab.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@EnableScheduling
 @ComponentScan("pl.coderslab")
 public class AppConfig implements WebMvcConfigurer {
 
